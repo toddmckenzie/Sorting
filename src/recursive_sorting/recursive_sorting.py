5d085d493966574ca7fdca9b2653b6a4a5db357e
Todd McKenzie <toddmckenzie@Todds-MacBook-Air.local>
2019-09-11 15:09:15 -0500

    merge sort recursive working

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,9 +1,13 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    # merged_arr = [0] * elements
+    merged_arr = []
     # TO-DO
     while len(arrA) > 0 or len(arrB) > 0:
+        # print(arrA)
+        # print(arrB)
+        # print(merged_arr)
         if len(arrA) > 0 and len(arrB) > 0:
             if arrA[0] > arrB[0]:
                 merged_arr.append(arrB[0])
@@ -24,33 +28,33 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
-    if len(arr) == 1:
-        return arr
-    else:
-        if len(arr) % 2 == 0:
-            merge( arr[0:len(arr)/2], arr[len(arr)/2:] )
-        else:
-            num = len(arr)/2 + .5
-            merge( arr[0:num], arr[num:] )
-    return arr
-
-
-print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
-
-# STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
-
-    return arr
-
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
+    if len(arr) > 1:
+        leftArray = arr[0:len(arr)//2]
+        rightArray = arr[len(arr)//2:]
+        left = merge_sort(leftArray)
+        right = merge_sort(rightArray)
+        arr = merge(left, right)
 
     return arr
 
 
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+# print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 
-    return arr
+# # STRETCH: implement an in-place merge sort algorithm
+# def merge_in_place(arr, start, mid, end):
+#     # TO-DO
+
+#     return arr
+
+# def merge_sort_in_place(arr, l, r): 
+#     # TO-DO
+
+#     return arr
+
+
+# # STRETCH: implement the Timsort function below
+# # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
+# def timsort( arr ):
+
+#     return arr
+
